4d0ac771fb6da4b0523ae78a5bdc7a498c2ebda1
Qvineox <yarlrusman@gmail.com>
2021-05-05 14:28:38 +0300

    Basic ANFIS created

--- main.py
+++ main.py
@@ -1,153 +1,6 @@
 import json
 
-global rules
-
-
-class InputValue:
-    def __init__(self, _value):
-        self.value = _value
-        self.memberships = exchange_rate_membership(self.value)
-        print(self)
-
-    def __str__(self):
-        return 'Input value {0} <= {1}'.format(round(self.value, 1), self.memberships)
-
-    def __repr__(self):
-        return self.__str__()
-
-
-class Rule:
-    def __init__(self, conditions, conclusion=None, veracity=None):
-        self.conditions = conditions
-        self.conclusion = conclusion
-        self.veracity = veracity
-
-    def __str__(self):
-        return 'Rule {0} => {1} ({2})'.format(self.conditions, self.conclusion, self.veracity)
-
-    def __repr__(self):
-        return self.__str__()
-
-
-# функция, определяющая к каким термам принадлежат входные переменные
-def exchange_rate_membership(x_value):
-    # границы нечеткого определения
-    if x_value <= 4:
-        value = 1.0
-        return {
-            'term': 'cheap',
-            'membership': round(value, 2)
-        }
-    if x_value >= 12:
-        value = 1.0
-        return {
-            'term': 'expensive',
-            'membership': round(value, 2)
-        }
-
-    # нечеткая принадлежность
-    _result = []
-    if 4 < x_value <= 8:
-        value = (8 - x_value) / 4
-        _result.append({
-            'term': 'cheap',
-            'membership': round(value, 2)
-        })
-    if 6 <= x_value <= 10:
-        if x_value <= 8:
-            value = (x_value - 6) / 2
-        else:
-            value = (10 - x_value) / 2
-        _result.append({
-            'term': 'balanced',
-            'membership': round(value, 2)
-        })
-    if x_value >= 9:
-        value = (x_value - 9) / 3
-        _result.append({
-            'term': 'expensive',
-            'membership': round(value, 2)
-        })
-    return _result
-
-
-# функция, вычислябщая итоговые степени истинности правил
-def input_evaluation(input_values):
-    memberships = []
-    for value in input_values:
-        memberships += value.memberships
-
-    print('> Received {0} input values for evaluation containing overall {1} memberships.'.format(
-        len(input_values),
-        len(memberships)
-    ))
-    print('Staring evaluation...')
-
-    _new_rules = []
-    for item_one in input_values[0].memberships:
-        term_one = item_one['term']
-        membership_one = item_one['membership']
-        for item_two in input_values[1].memberships:
-            term_two = item_two['term']
-            membership_two = item_two['membership']
-            for item_three in input_values[2].memberships:
-                term_three = item_three['term']
-                membership_three = item_three['membership']
-                for item_four in input_values[3].memberships:
-                    term_four = item_four['term']
-                    membership_four = item_four['membership']
-
-                    # собираем новое правило
-                    new_rule = Rule(conditions=[term_one, term_two, term_three, term_four],
-                                    veracity=min(
-                                        [membership_one, membership_two, membership_three, membership_four]))
-                    _new_rules.append(new_rule)
-
-    print('> Evaluation completed. Attained {0} new computed rules.'.format(len(_new_rules)))
-
-    for rule in _new_rules:
-        print(rule)
-
-    return _new_rules
-
-
-# нормализация вычисленных тоговых степеней предпосылок
-def generated_rules_normalization(computed_rules):
-    print('Starting normalization of computed rules...')
-    for rule in computed_rules:
-        rule.veracity = rule.veracity / sum(list(map(lambda x: x.veracity, computed_rules)))
-
-    print('> Normalization completed.')
-    for rule in computed_rules:
-        print(rule)
-
-    return computed_rules
-
-
-def generated_rules_determination(normalized_computed_rules):
-    print('Staring determination of the final degrees of veracity...')
-
-
-def fill_rules():
-    rules = {'rules': []}
-    terms = ['cheap', 'balanced', 'expensive']
-
-    for i in range(3):
-        for j in range(3):
-            for k in range(3):
-                for l in range(3):
-                    rules['rules'].append({
-                        'conditions': [
-                            terms[i],
-                            terms[j],
-                            terms[k],
-                            terms[l],
-                        ],
-                        'conclusion': 'none'
-                    })
-
-    with open('rules.json', 'w') as json_file:
-        json.dump(rules, json_file)
+input_variables = []
 
 
 def load_rules():
@@ -157,25 +10,131 @@ def load_rules():
         data = json.load(json_file)
 
         for rule in data['rules']:
-            _rules.append(Rule(conditions=rule['conditions'], conclusion=rule['conclusion']))
+            _rules.append({
+                'conditions': rule['conditions'],
+                'conclusion': rule['conclusion']
+            })
 
     return _rules
 
 
+class FuzzificationNeuron:
+    def __init__(self, center, width):
+        self.center = float(center)
+        self.width = float(width)
+
+    def quantify(self, value):
+        result = round((1 / (1 + pow(pow((value - self.center), 2), self.width))), 4)
+        return result
+
+
+class AgregationNeuron:
+    @staticmethod
+    def compute(values):
+        result = min(values)
+        return result
+
+
+class NormalizationNeuron:
+    def __init__(self, total):
+        self.total = round(total, 4)
+
+    def normalize(self, value):
+        result = value / self.total
+        return round(result, 4)
+
+
+class DefuzzifactionNeuron:
+    global input_variables
+
+    def __init__(self):
+        self.rules = load_rules()
+
+    def defuzzification(self, value, terms):
+        power_rule = next(
+            item for item in self.rules if item["conditions"] == terms)
+
+        x1, x2, x3, x4 = tuple(input_variables)
+
+        result = value * eval(power_rule['conclusion'])
+        return round(result, 4)
+
+
+# Layer 1 is the input variables, fuzzy sets. Each node in layer 1 fits a function parameter.
+def fuzzification_layer():
+    global input_variables
+    print("0 LAYER: {0}".format(input_variables))
+
+    cheap_fuzzificator = FuzzificationNeuron(9, 1.9)
+    fair_fuzzificator = FuzzificationNeuron(10.5, 1.3)
+    expensive_fuzzificator = FuzzificationNeuron(12, 1.5)
+
+    results = []
+
+    # 12 Nodes of Layer 1
+    for x in input_variables:
+        results.append([
+            {'membership': cheap_fuzzificator.quantify(x), 'term': 'cheap'},
+            {'membership': fair_fuzzificator.quantify(x), 'term': 'fair'},
+            {'membership': expensive_fuzzificator.quantify(x), 'term': 'expensive'}
+        ])
+
+    print("1 LAYER: {0}".format(results))
+    return results
+
+
+# Layer 2 computes any two memberships acquired by the fuzzy sets to characterize the fuzzy rules.
+def agregation_layer(fuzzificated_values):
+    agregator = AgregationNeuron
+
+    results = []
+    # 81 Node of Layer 2
+    for x1 in fuzzificated_values[0]:
+        for x2 in fuzzificated_values[1]:
+            for x3 in fuzzificated_values[2]:
+                for x4 in fuzzificated_values[3]:
+                    results.append(
+                        {'value': agregator.compute(
+                            [x1['membership'], x2['membership'], x3['membership'], x4['membership']]),
+                            'terms': [x1['term'], x2['term'], x3['term'], x4['term']]})
+
+    print("2 LAYER: (amount: {0}) {1}".format(len(results), results))
+    return results
+
+
+# Each node in layer 3 is fixed or non-adaptive. Outputs of this layer are called normalized firing strengths.
+def normalization_layer(agregated_values):
+    normalizator = NormalizationNeuron(sum(item['value'] for item in agregated_values))
+
+    results = []
+    # 81 Node of Layer 3
+    for x in agregated_values:
+        results.append({'value': normalizator.normalize(x['value']), 'terms': x['terms']})
+
+    print("3 LAYER: (sum: {0}) {1}".format(normalizator.total, results))
+    return results
+
+
+# Each node fits an output in layer 4. The parameters in layer 4 are returned as the following parameters.
+def defuzzification_layer(normalized_variables):
+    defuzzificator = DefuzzifactionNeuron()
+
+    results = []
+    for x in normalized_variables:
+        results.append(defuzzificator.defuzzification(x['value'], x['terms']))
+
+    print("4 LAYER: (amount: {0}) {1}".format(len(results), results))
+    return results
+
+
+# Layer 5 is the last layer and contains only one node. In this layer, the single node is a non-adaptive or fixed node.
+def summary_layer(defuzzificated_variables):
+    result = round(sum(defuzzificated_variables), 2)
+
+    print("5 LAYER: {0}".format(result))
+    return result
+
+
 if __name__ == '__main__':
-    debug = True
-    rules = load_rules()
-    inputValues = []
-
-    if debug:
-        inputValues.append(InputValue(float(5.67)))
-        inputValues.append(InputValue(float(6.2)))
-        inputValues.append(InputValue(float(8.3)))
-        inputValues.append(InputValue(float(9.72)))
-    else:
-        for i in range(4):
-            print('Enter {0} day exchange rate (0-12):'.format(i + 1))
-            inputValues.append(InputValue(float(input())))
-
-    # print(inputValues)
-    generated_rules_normalization(input_evaluation(inputValues))
+    input_variables = [11.5, 11.9, 10.2, 10.6]
+    summary_layer(defuzzification_layer(normalization_layer(agregation_layer(fuzzification_layer()))))
